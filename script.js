bd9abbff42b552f6b7de8d322ad4a217951f10eb
Vignesh-1523 <gabrielmac0912@gmail.com>
2024-01-11 03:51:39 -0800

    empty files

---
+++ script.js
@@ -0,0 +1,12 @@
+let comName = document.getElementById('name');
+let comNo = document.getElementById('No');
+let comLink = document.getElementById('link');
+let comWeb = document.getElementById('web');
+let comPh = document.getElementById('ph');
+let comE = document.getElementById('mail');
+
+let details = document.getElementById('details');
+
+let company_arr = [];
+
+
